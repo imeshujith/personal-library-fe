8048ee6cf71a0d1303aad82c9c7f9cbf6cda33d2
imesh <imeshujith@gmail.com>
2024-09-15 17:39:44 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/Login';
+import SignupPage from './pages/Signup';
+import DashboardLayout from './components/DashboardLayout';
+import HomePage from './pages/HomePage'
+import ProtectedRoute from './components/ProtectedRoute';
+import { useSelector } from 'react-redux';
 
-function App() {
+const App = () => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/" element={<DashboardLayout />}>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute isAuthenticated={isAuthenticated}>
+            <HomePage />
+          </ProtectedRoute>
+        }
+      />
+      </Route>
+    </Routes>
+  </Router>
+  )
+};
 
 export default App;
